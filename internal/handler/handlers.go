fe471af18c819cf86604bebfd2b0cf266b965a2a
Denis Smirnov <denis.smirnov1985@gmail.com>
2025-08-07 22:05:55 +0300

    тесты - ок

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,32 +1,16 @@
 package handler
 
 import (
-	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
 	"reflect"
-	"database/sql"
+
+	"github.com/go-chi/chi/v5"
 )
 
-
-func InsertURL(dataBase *sql.DB, original, short string) error {
-    insertUser := `INSERT INTO urls (original, short) VALUES (?, ?)`
-    _, err := dataBase.Exec(insertUser, original, short)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-
-func GetURL(dataBase *sql.DB, short string) (string, error) {
-    URL := `SELECT original from urls WHERE short = (?)`
-	var original string
-
-    _ = dataBase.QueryRow(URL, short).Scan(&original)
-
-	return original, nil
-}
+var (
+	OriginalAndShotArray = map[string]string{}
+)
 
 func CreateRouter() http.Handler {
 	router := chi.NewRouter()
@@ -51,14 +35,7 @@ func createShortURL(res http.ResponseWriter, req *http.Request) {
 
 	defer req.Body.Close()
 
-	dataBase, _ := sql.Open("sqlite", "./shortener_database.db")
-	InsertURL(dataBase, string(bodyBytes), "EwHXdJfB")
-
-	defer dataBase.Close()
-
-	// проверка на уже существование в БД
-	// дополниеть реализацией связи с БД
-	// ShortenerArray["EwHXdJfB"] = string(bodyBytes)
+	OriginalAndShotArray["EwHXdJfB"] = string(bodyBytes)
 
 	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusCreated)
@@ -72,19 +49,14 @@ func getOriginalURL(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	dataBase, _ := sql.Open("sqlite", "./shortener_database.db")
-	defer dataBase.Close()
-
-
 	shortURL := chi.URLParam(req, "short_url")
 	defer req.Body.Close()
 
-	headerLocation, _ := GetURL(dataBase, shortURL)
-
-	// if !ok {
-	// 	res.WriteHeader(http.StatusNotFound)
-	// 	return
-	// }
+	headerLocation, ok := OriginalAndShotArray[shortURL]
+	if !ok {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
 	res.Header().Add("Location", headerLocation)
 	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusTemporaryRedirect)
