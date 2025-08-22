e15b5d73d51c18c3a8c4edaee04c036a1c202c85
Denis Smirnov <denis.smirnov1985@gmail.com>
2025-08-23 00:09:10 +0300

    инкремент 5: переменыне окружения: откат на решение инкремента 5

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,8 +10,7 @@ import (
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
-	LogLevel      string
-	// LogLevel      string `env:"LOG_LEVEL"`
+	LogLevel      string `env:"LOG_LEVEL"`
 }
 
 func (a *Config) Parse() {
@@ -20,18 +19,12 @@ func (a *Config) Parse() {
 	// первый приоритет - из переменных окружения
 	_ = env.Parse(a)
 
-	// второй приоритет - аргументы командной строки / значение по умолчанию
-	if a.ServerAddress == "" {
-		flag.StringVar(&a.ServerAddress, "a", "localhost:8080", "URL")
+	if a.ServerAddress != "" && a.BaseURL != "" {
+		return
 	}
-	if a.BaseURL == "" {
-		flag.StringVar(&a.BaseURL, "b", "http://localhost:8080", "base URL")
-	}
-
-	a.LogLevel = "info"
-	// if a.LogLevel == "" {
-	// 	flag.StringVar(&a.LogLevel, "c", "info", "log level")
-	// }
+	// второй приоритет - из аргументов командной строки
+	flag.StringVar(&a.ServerAddress, "a", "localhost:8080", "URL")
+	flag.StringVar(&a.BaseURL, "b", "http://localhost:8080", "base URL")
 
 	flag.Parse()
 }
