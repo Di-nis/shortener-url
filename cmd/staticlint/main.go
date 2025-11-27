072bcd13f2436da67632c57778704f23a57a7f2c
Denis Smirnov <denis.smirnov1985@gmail.com>
2025-11-27 14:32:24 +0300

    iter20. + multichecker

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,92 +1,14 @@
+// Package main предоставляет точку входа для запуска
+// набора статических анализаторов как единого инструмента. Пакет предназначен
+// для сборки бинаря, подобного `go vet`, который запускает сразу множество
+// анализаторов из `golang.org/x/tools/go/analysis/passes`, `honnef.co/go/tools`
+// (stylecheck) и `honnef.co/go/staticcheck` (staticcheck).
 package main
 
 import (
-	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/multichecker"
-	"honnef.co/go/tools/staticcheck"
-	"strings"
-
-	"golang.org/x/tools/go/analysis/passes/asmdecl"
-	"golang.org/x/tools/go/analysis/passes/assign"
-	"golang.org/x/tools/go/analysis/passes/atomic"
-	"golang.org/x/tools/go/analysis/passes/bools"
-	"golang.org/x/tools/go/analysis/passes/buildtag"
-	"golang.org/x/tools/go/analysis/passes/cgocall"
-	"golang.org/x/tools/go/analysis/passes/composite"
-	"golang.org/x/tools/go/analysis/passes/copylock"
-	"golang.org/x/tools/go/analysis/passes/ctrlflow"
-	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
-	"golang.org/x/tools/go/analysis/passes/errorsas"
-	"golang.org/x/tools/go/analysis/passes/httpresponse"
-	"golang.org/x/tools/go/analysis/passes/ifaceassert"
-	"golang.org/x/tools/go/analysis/passes/loopclosure"
-	"golang.org/x/tools/go/analysis/passes/lostcancel"
-	"golang.org/x/tools/go/analysis/passes/nilfunc"
-	"golang.org/x/tools/go/analysis/passes/nilness"
-	"golang.org/x/tools/go/analysis/passes/printf"
-	"golang.org/x/tools/go/analysis/passes/shift"
-	"golang.org/x/tools/go/analysis/passes/sigchanyzer"
-	"golang.org/x/tools/go/analysis/passes/sortslice"
-	"golang.org/x/tools/go/analysis/passes/stdmethods"
-	"golang.org/x/tools/go/analysis/passes/stringintconv"
-	"golang.org/x/tools/go/analysis/passes/structtag"
-	"golang.org/x/tools/go/analysis/passes/testinggoroutine"
-	"golang.org/x/tools/go/analysis/passes/tests"
-	"golang.org/x/tools/go/analysis/passes/unmarshal"
-	"golang.org/x/tools/go/analysis/passes/unreachable"
-	"golang.org/x/tools/go/analysis/passes/unsafeptr"
-	"golang.org/x/tools/go/analysis/passes/unusedresult"
-
-	"github.com/Di-nis/shortener-url/pkg/errcheckexit"
+	"github.com/Di-nis/shortener-url/internal/multichecker"
 )
 
 func main() {
-	// Все анализаторы из passes
-	analyzersPasses := []*analysis.Analyzer{
-		asmdecl.Analyzer,
-		assign.Analyzer,
-		atomic.Analyzer,
-		bools.Analyzer,
-		buildtag.Analyzer,
-		cgocall.Analyzer,
-		composite.Analyzer,
-		copylock.Analyzer,
-		ctrlflow.Analyzer,
-		deepequalerrors.Analyzer,
-		errorsas.Analyzer,
-		httpresponse.Analyzer,
-		ifaceassert.Analyzer,
-		loopclosure.Analyzer,
-		lostcancel.Analyzer,
-		nilness.Analyzer,
-		nilfunc.Analyzer,
-		printf.Analyzer,
-		shift.Analyzer,
-		sigchanyzer.Analyzer,
-		sortslice.Analyzer,
-		stdmethods.Analyzer,
-		stringintconv.Analyzer,
-		structtag.Analyzer,
-		testinggoroutine.Analyzer,
-		tests.Analyzer,
-		unmarshal.Analyzer,
-		unreachable.Analyzer,
-		unsafeptr.Analyzer,
-		unusedresult.Analyzer,
-	}
-
-	IDSAGroup := "SA"
-	var analyzersSAGroup []*analysis.Analyzer
-
-	for _, v := range staticcheck.Analyzers {
-		if strings.HasPrefix(v.Analyzer.Name, IDSAGroup) {
-			analyzersSAGroup = append(analyzersSAGroup, v.Analyzer)
-		}
-	}
-
-	analyzersAll := append(analyzersPasses, analyzersSAGroup...)
-
-	multichecker.Main(
-		analyzersAll...,
-	)
+	multichecker.Run()
 }
